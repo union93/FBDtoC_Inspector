6a25b30fe4c9fd2b0c4f2dc654c49be90c6c04e0
union93 <union93@konkuk.ac.kr>
2021-03-21 23:51:02 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,8 @@
+#define BUFLEN (100)
+int dataarrayl[BUFLEN], i;
+/* initialize buffer */
+for (i=l; i<=BUFLEN; i++)
+data_array[i] = 0;
+dataarray[BUF_LEN] = i;
+/* wrong */
+/* wrong, BUFLEN is outside of the array */
